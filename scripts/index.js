f64cd77e418a89dcd57e494e97dadbca684d475b
Steven Migliorato <smigliorato@gmail.com>
2015-05-05 18:48:18 -0500

    removed useless import

--- scripts/index.js
+++ scripts/index.js
@@ -3,8 +3,6 @@ import {HashLocation, run} from 'react-router';
 import Routes from './Routes';
 import './index.scss';
 
-import MessageActions from './actions/MessageActions';
-
 run(Routes, function(Handler, state) {
   React.render(<Handler {...state} />, document.getElementById('root'));
 });
